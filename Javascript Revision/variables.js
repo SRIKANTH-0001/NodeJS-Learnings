daf862b2774ac6a140620777383a3793d0659171
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-09-09 06:28:59 +0530

    Variables in Js

---
+++ Javascript Revision/variables.js
@@ -0,0 +1,37 @@
+
+//3 ways to declare variables in Js
+
+//Creating the variable using let
+
+let x=141;
+let y=23;
+let z=x+y;
+
+console.log(` Addition of Given Numbers: ${z}`);
+console.log("------------------------------");
+
+
+//Creating variables using const
+
+const _fName="Sri";
+const _mName="kanth"
+const $lName=" K";
+
+console.log(`The fullName is : ${_fName+_mName+$lName}`);
+console.log("------------------------------");
+
+//using depricated var 
+var x1=5;
+var x1=4;
+var y1=3;
+var z1=x1-y1;
+
+console.log(`The subtraction of given values : ${z1}`);
+console.log("------------------------------");
+
+
+
+
+
+
+
--- myFirstServer.js
+++ myFirstServer.js
@@ -3,4 +3,4 @@ let http=require('http');
 http.createServer((req,res)=>{
     res.writeHead(200,{"content-type":'text/html'});
     res.end('Hello World');
-}).listen(5173);
+}).listen(5000);
