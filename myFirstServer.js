dda6da300d37874336ae24cce2c3ed8ddcf9b26c
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-09-13 18:15:40 +0530

    Use of EventLoop in Nodejs

--- myFirstServer.js
+++ myFirstServer.js
@@ -1,65 +1,22 @@
-const fs=require('fs');
+//Execution of NodeJs with event loop concepts
 
-//Ex-1:
-    //Blocking File read(synchronous)...
+//1.synchronous
+console.log("Sunc Start");
 
-    // console.log("Blocking File reading is started!...");
-    // const data=fs.readFileSync('sample.txt','utf8');
-    // console.log(data);
-    // console.log("Blocking File reading is Completed!...");
+//2.nextTick()
+process.nextTick(()=>console.log("NextTick function..."));
 
+//3.Timer functions
+setTimeout(() => {
+    console.log("Timer function");
+}, 0);
 
-    //Non-Blocking file read (Asynchronous)
-    console.log("-----___________________________________________-----");
-    
+//3.From microTask Queue -Promises
+Promise.resolve().then(()=>console.log("Promises code..."));
 
-    console.log("Non-Blocking file reading is started!...");
+//4. setImmediate callbacks
+setImmediate(()=>console.log("setImmediate Callback function..."))
 
-    const result=fs.readFile('sample.txt','utf8',(err,data)=>{
-        if(err) throw err;
-        console.log(data)
-    })
-
-    console.log("In Non-Blocking file read this line should run before file read!..");
-    console.log("-----___________________________________________-----");
-
-    
-//Ex-2
-
-    //Blocking File read(synchronous)..readFileSync() blocks the timeout()
-        // console.log('1. Before reading');
-
-        // setTimeout(() => {
-        //     console.log('4. Timer executed');
-        // }, 0);
-
-        // const data1=fs.readFileSync('sample.txt', 'utf8');
-
-        // console.log('2. File reading completed');
-        // console.log('3. After reading');
-
-    //Non-Blocking file read (Asynchronous)-readFile() not blocks the timeout()
-        console.log("-----___________________________________________-----");
-        
-
-        // console.log('1. Before reading');
-
-        // setTimeout(() => {
-        //     console.log('4. Timer executed');
-        // }, 0);
-
-        // fs.readFile('sample.txt', 'utf8', (err, data) => {
-        //     if (err) throw err;
-        //     console.log('3. File reading completed');
-        // });
-
-        // console.log('2. After starting reading');
-
-
-
-    
-
-
-
-
+//1.synchronous
+console.log("Sync End");
 
