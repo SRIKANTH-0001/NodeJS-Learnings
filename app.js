4f926b67607c62f9e4c588856bece25d00bc2b49
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-09-18 06:24:45 +0530

    Topics covered: Node NPM Node package.json Node NPM Scripts

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-import {add,mul} from './utils.js'
-import minus from './utils.js'
+//Today I have comleted Two topics
+//That happened in full of CLI-(Command line Interface)
+    // Topic 1: NPM
+        /**
+         *  1.1 To install any package
+         *          -- npm install <Package-Name>
+         * 
+         *  1.2 To update speecific package
+         *          -- npm update <Package-Name>
+         *      
+         *      1.2.1 To update all packages
+         *          -- npm update
+         * 
+         *  1.3 To uninstall a specific Package
+         *          -- npm uninstall <Package-Name>
+         */
 
-//Named Es modules Import 
-console.log("Addition of 4,5 :"+"  "+add(4,5));
-console.log("Multiplication of 4 and 5 :"+mul(4,5));
+    //Topic : 2 package.json
+        /**
+         *  What is package.json?
+         *      It contains information about your app, such as its name, version, dependencies, scripts, and more.
+         * 
+         *    2.1 To create package.json file?
+         *         --  npm init
+         *         note:
+         *              After executing that command it asks so many questions we can use
+         *         -- npm init -y
+         */
 
-//Default Export of ES modules Import 
-console.log("Subtraction of 4,5 :"+"  "+minus(4,5));
+    //Topic : 3 Npm scrits
+        /**
+         *  Intro:
+         *          These scripts are commands used for automate tasks like,
+         *      In package.json ,at scripts if i give..,
+         *          "start":"node app.js"
+         * 
+         *      Then,in command line ,Instead of "node app.js" we can use "npm start" that's it...
+         */
+
+
+
+
+
+
+
 
 
 
